f73cb6da78644903093fe20bbd043f08fdb096b9
marciogabrielsf <marciogdsf@gmail.com>
2023-11-06 20:43:30 -0300

    deactivated unique

--- src/routes/vouchers.routes.ts
+++ src/routes/vouchers.routes.ts
@@ -13,50 +13,35 @@ voucherRoutes.post('/voucher/create', verifyJWT, (async (req: Request, res: Resp
     return res.status(422).json({ message: 'Missing parameters' })
   }
 
-  const voucherExist = await prisma.voucher.findMany({
-    where: {
-      voucherNumber,
-      userId: id
-    }
-  })
-
-  if (voucherExist.length !== 0) {
-    return res.status(422).json({ message: 'O Número do voucher já está cadastrado' })
-  }
-
-  const orderExist = await prisma.voucher.findMany({
-    where: {
-      orderNumber,
-      userId: id
-    }
-  })
-
-  if (orderExist.length !== 0) {
-    return res.status(422).json({ message: 'O Número do pedido já está cadastrado' })
-  }
-
-  await prisma.voucher.create({
-    data: {
-      userId: id,
-      value,
-      voucherDate,
-      voucherNumber,
-      company,
-      orderNumber
-    },
-    include: {
-      user: true
-    }
-  }).then(() => {
-    res.status(201).json({
-      code: 'voucher.created-success',
-      message: 'Voucher Criado com sucesso!'
+  await prisma.voucher
+    .create({
+      data: {
+        userId: id,
+        value,
+        voucherDate,
+        voucherNumber,
+        company,
+        orderNumber
+      },
+      include: {
+        user: true
+      }
     })
-  }).catch(err => {
-    res.status(500).json({ message: 'Erro ao Criar o Voucher, tente novamente mais tarde', error: err.message })
-  })
+    .then(() => {
+      res.status(201).json({
+        code: 'voucher.created-success',
+        message: 'Voucher Criado com sucesso!'
+      })
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .json({
+          message: 'Erro ao Criar o Voucher, tente novamente mais tarde',
+          error: err.message
+        })
+    })
 }) as RequestHandler)
-
 
 voucherRoutes.get('/voucher/getlist', verifyJWT, (async (req: Request, res: Response) => {
   const id = req.body.id
@@ -68,9 +53,10 @@ voucherRoutes.get('/voucher/getlist', verifyJWT, (async (req: Request, res: Resp
   })
   if (vouchers.length === 0) {
     res.status(422).json({ message: 'Nenhum voucher foi encontrado' })
-  } else { res.status(200).json({ vouchers }) }
+  } else {
+    res.status(200).json({ vouchers })
+  }
 }) as RequestHandler)
-
 
 voucherRoutes.put('/voucher/update/:id', verifyJWT, (async (req: Request, res: Response) => {
   const id = req.body.id
@@ -78,12 +64,10 @@ voucherRoutes.put('/voucher/update/:id', verifyJWT, (async (req: Request, res: R
   const { value, company, orderNumber, voucherDate, voucherNumber } = req.body
 
   if (!value || !company || !orderNumber || !voucherDate || !voucherNumber) {
-
     return res.status(422).json({ message: 'Missing parameters' })
   }
 
-
-  var checkForHexRegExp = new RegExp("^[0-9a-fA-F]{24}$")
+  const checkForHexRegExp = /^[0-9a-fA-F]{24}$/
   if (!checkForHexRegExp.test(voucherID)) {
     return res.status(422).json({ message: 'Invalid ID' })
   }
@@ -102,31 +86,37 @@ voucherRoutes.put('/voucher/update/:id', verifyJWT, (async (req: Request, res: R
     return res.status(404).json({ message: 'Voucher não encontrado' })
   }
 
-  await prisma.voucher.update({
-    where: {
-      id: getVoucher.id
-    },
-    data: {
-      company,
-      value,
-      orderNumber,
-      voucherDate,
-      voucherNumber
-    }
-  }).then(() => {
-    res.status(200).json({ message: 'Voucher foi atualizado com sucesso!' })
-  }).catch(err => {
-    res.status(500).json({ message: 'Erro ao Atualizar o Voucher, tente novamente mais tarde', error: err.message })
-  })
-
+  await prisma.voucher
+    .update({
+      where: {
+        id: getVoucher.id
+      },
+      data: {
+        company,
+        value,
+        orderNumber,
+        voucherDate,
+        voucherNumber
+      }
+    })
+    .then(() => {
+      res.status(200).json({ message: 'Voucher foi atualizado com sucesso!' })
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .json({
+          message: 'Erro ao Atualizar o Voucher, tente novamente mais tarde',
+          error: err.message
+        })
+    })
 }) as RequestHandler)
-
 
 voucherRoutes.delete('/voucher/delete/:id', verifyJWT, (async (req: Request, res: Response) => {
   const id = req.body.id
   const voucherID = req.params.id
 
-  var checkForHexRegExp = new RegExp("^[0-9a-fA-F]{24}$")
+  const checkForHexRegExp = /^[0-9a-fA-F]{24}$/
   if (!checkForHexRegExp.test(voucherID)) {
     return res.status(422).json({ message: 'Invalid ID' })
   }
@@ -145,17 +135,18 @@ voucherRoutes.delete('/voucher/delete/:id', verifyJWT, (async (req: Request, res
     return res.status(422).json({ message: 'Invalid Token to update this voucher.' })
   }
 
-  await prisma.voucher.delete({
-    where: {
-      id: getVoucher.id
-    }
-  }).then(() => {
-    res.status(200).json({ message: 'O Voucher foi Deletado com sucesso!' })
-  }).catch(err => {
-    res.status(500).json({ message: 'Erro ao deletar o Voucher' })
-  })
-
+  await prisma.voucher
+    .delete({
+      where: {
+        id: getVoucher.id
+      }
+    })
+    .then(() => {
+      res.status(200).json({ message: 'O Voucher foi Deletado com sucesso!' })
+    })
+    .catch(() => {
+      res.status(500).json({ message: 'Erro ao deletar o Voucher' })
+    })
 }) as RequestHandler)
-
 
 export default voucherRoutes
